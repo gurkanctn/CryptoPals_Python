ed1e1455ef97dbac191cd5bd3561d47bec23ec2f
My Name <gurkancetin@gmail.com>
2017-06-14 13:02:54 +0300

    Add files via upload

--- Challenge-2.py
+++ Challenge-2.py
@@ -36,12 +36,11 @@ def deHex(str1):  #returns a string in Base64
             myResult2+= chr(myResult[i])
     return myResult2
 
-def deHex2(str1):  #Converts a string of Hex to an array of Base64
+def deHex2(str1):  #Converts a string of Hex to an array of Base64 bytes
     inputBin = binascii.unhexlify(str1)
-    #myResult = binascii.b2a_base64(inputBin) #Temporary, work variable
     myResult2 = []
     for i in range(len(inputBin)):
-        #print (chr(myResult[i]), ":", myResult[i])
+        print(inputBin[i])
         if inputBin[i]!=10:
             myResult2.append(inputBin[i])
     return myResult2
@@ -53,26 +52,19 @@ def myXOR(str1,str2): #returns a string in Base64 of two strings of Base64
     temp2 = deHex2(str2)
     print("temp2  :",temp2)
     if len(str1)==len(str2):   
-        for i in range(len(temp1)):  # TBD
+        for i in range(len(temp1)):
             Result.append(temp1[i] ^ temp2[i])
 #            print(chr(Result[i]))
     else:
         print( "lengths must be equal")
     return (Result)
 
-#Result = binascii.a2b_base64(Result)
-"""Convert a block of base64 data back to binary and return the binary data. More than one line may be passed at a time.
-binascii.b2a_base64(data, *, newline=True)
-Convert binary data to a line of ASCII characters in base64 coding. The return value is the converted line, including a newline char if newline is true. The output of this function conforms to RFC 3548.
-Changed in version 3.6: Added the newline parameter."""
-
-#_____________________________________________
+#__________________CHALLENGE___________________
 
 input1="1c0111001f010100061a024b53535009181c"
 print ("input1  : \n  ", input1)
 input2 = "686974207468652062756c6c277320657965"
 print ("input2  : \n  ", input2)
-
 Answer = "746865206b696420646f6e277420706c6179"
 print ("Answer  : \n  ", Answer)
 #______________________________________________
@@ -88,17 +80,22 @@ for i in range (len(myAnswer)):
 if len(myAnswer2) % 4:
    myAnswer2 += '=' * (4 - len(myAnswer2) % 4) 
 
-print ("myAnswer in ASCII: \n  ",myAnswer2)
+print ("myAnswer in ASCII: \n",myAnswer2)
 
 #for i in range (len(myAnswer)):
 #    myAnswer3+=binascii.a2b_base64(myAnswer2[i])
 
-#print ("myAnswer in b64: \n  ",myAnswer3)
+#print ("myAnswer in b64: \n",myAnswer3)
     
-myAnswer3=binascii.a2b_base64(myAnswer2)   #INCORRECT PADDING!!!
-myAnswer3=binascii.b2a_base64(myAnswer3)   #NOT CORRECT!
 
-print ("my FINAL ANSWER in BASE64 readable: ",myAnswer3)
+myAnswer3=binascii.a2b_base64(myAnswer2)   #string to B64
+myAnswer3=binascii.hexlify(myAnswer3)      #B64 to Hex
+#myAnswer3=binascii.b2a_base64(myAnswer3)   #NOT CORRECT!
+myAnswer3=binascii.b2a_base64(myAnswer3)
+myAnswer4=binascii.hexlify(myAnswer3).decode('ASCII')
+myAnswer4=myAnswer3
+
+print ("my FINAL ANSWER: \n",myAnswer4)
 
 # CHECK IF CHALLENGE COMPLETED SUCCESSFULLY
 if myAnswer3 == Answer:
